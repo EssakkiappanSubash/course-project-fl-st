c22fe9f2c155dc2591a88deeab6713b7019194c7
EssakkiappanSubash <125815464+EssakkiappanSubash@users.noreply.github.com>
2024-02-07 10:46:23 +0530

    Add files via upload

---
+++ dom/Dicee Challenge-Starting Files/index.js
@@ -0,0 +1,30 @@
+
+// Player 1 dice
+var randomNumber1 = Math.floor(Math.random() * 6) + 1; // random numeber 1-6
+
+var randomDiceImage = "dice" + randomNumber1 + ".png"; //  dice1.png - dice6.png
+
+var randomImageSource1 = "./images/" + randomDiceImage; //  ./images/dice1.png - ./images/dice6.png
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource1);
+
+
+
+// Player 2 dice
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomImageSource2 = "./images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);   
+
+
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 1 Wins!";
+} else if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins!";
+} else {
+    document.querySelector("h1").innerHTML = "Draw!";
+}
